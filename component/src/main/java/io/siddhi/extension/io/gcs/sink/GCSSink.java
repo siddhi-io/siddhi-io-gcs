a236e31499cb817e92ec74ff43bdcc7cd12616ae
CharukaK <charukakarunanayake@gmail.com>
2019-09-09 09:42:34 +0530

    Project initialization

---
+++ component/src/main/java/io/siddhi/extension/io/gcs/sink/GCSSink.java
@@ -0,0 +1,160 @@
+package io.siddhi.extension.io.gcs.sink;
+
+import io.siddhi.annotation.Example;
+import io.siddhi.annotation.Extension;
+import io.siddhi.core.config.SiddhiAppContext;
+import io.siddhi.core.exception.ConnectionUnavailableException;
+import io.siddhi.core.stream.ServiceDeploymentInfo;
+import io.siddhi.core.stream.output.sink.Sink;
+import io.siddhi.core.util.config.ConfigReader;
+import io.siddhi.core.util.snapshot.state.State;
+import io.siddhi.core.util.snapshot.state.StateFactory;
+import io.siddhi.core.util.transport.DynamicOptions;
+import io.siddhi.core.util.transport.OptionHolder;
+import io.siddhi.query.api.definition.StreamDefinition;
+
+/**
+ * This is a sample class-level comment, explaining what the extension class does.
+ */
+
+/**
+ * Annotation of Siddhi Extension.
+ * <pre><code>
+ * eg:-
+ * {@literal @}Extension(
+ * name = "The name of the extension",
+ * namespace = "The namespace of the extension",
+ * description = "The description of the extension (optional).",
+ * //Sink configurations
+ * parameters = {
+ * {@literal @}Parameter(name = "The name of the first parameter", type = "Supprted parameter types.
+ *                              eg:{DataType.STRING,DataType.INT, DataType.LONG etc},dynamic=false ,optinal=true/false ,
+ *                              if optional =true then assign default value according the type")
+ *   System parameter is used to define common extension wide
+ *              },
+ * examples = {
+ * {@literal @}Example({"Example of the first CustomExtension contain syntax and description.Here,
+ *                      Syntax describe default mapping for SourceMapper and description describes
+ *                      the output of according this syntax},
+ *                      }
+ * </code></pre>
+ */
+
+@Extension(
+        name = "google cloud storage",
+        namespace = "sink",
+        description = " ",
+        parameters = {
+                /*@Parameter(name = " ",
+                        description = " " ,
+                        dynamic = false/true,
+                        optional = true/false, defaultValue = " ",
+                        type = {DataType.INT, DataType.BOOL, DataType.STRING, DataType.DOUBLE,etc }),
+                        type = {DataType.INT, DataType.BOOL, DataType.STRING, DataType.DOUBLE, }),*/
+        },
+        examples = {
+                @Example(
+                        syntax = " ",
+                        description = " "
+                )
+        }
+)
+
+// for more information refer https://siddhi-io.github.io/siddhi/documentation/siddhi-5.x/query-guide-5.x/#sink
+
+public class GCSSink extends Sink {
+
+    /**
+     * Returns the list of classes which this sink can consume.
+     * Based on the type of the sink, it may be limited to being able to publish specific type of classes.
+     * For example, a sink of type file can only write objects of type String .
+     * @return array of supported classes , if extension can support of any types of classes
+     * then return empty array .
+     */
+    @Override
+    public Class[] getSupportedInputEventClasses() {
+            return new Class[0];
+    }
+
+    /**
+     * Returns a list of supported dynamic options (that means for each event value of the option can change) by
+     * the transport
+     *
+     * @return the list of supported dynamic option keys
+     */
+    @Override
+    public String[] getSupportedDynamicOptions() {
+            return new String[0];
+    }
+
+    /**
+     * The initialization method for {@link Sink}, will be called before other methods. It used to validate
+     * all configurations and to get initial values.
+     * @param streamDefinition        containing stream definition bind to the {@link Sink}
+     * @param optionHolder            Option holder containing static and dynamic configuration related
+     *                                to the {@link Sink}
+     * @param configReader            to read the sink related system configuration.
+     * @param siddhiAppContext        the context of the {@link io.siddhi.query.api.SiddhiApp} used to
+     *                                get siddhi related utility functions.
+     * @return StateFactory for the Function which contains logic for the updated state based on arrived events.
+     */
+    @Override
+    protected StateFactory init(StreamDefinition streamDefinition, OptionHolder optionHolder, ConfigReader configReader,
+                                SiddhiAppContext siddhiAppContext) {
+        return null;
+    }
+
+    /**
+     * This method will be called when events need to be published via this sink
+     * @param payload         payload of the event based on the supported event class exported by the extensions
+     * @param dynamicOptions  holds the dynamic options of this sink and Use this object to obtain dynamic options.
+     * @param state           current state of the sink
+     * @throws ConnectionUnavailableException if end point is unavailable the ConnectionUnavailableException thrown
+     *                                        such that the  system will take care retrying for connection
+     */
+    @Override
+    public void publish(Object payload, DynamicOptions dynamicOptions, State state)
+                                                                              throws ConnectionUnavailableException {
+
+    }
+
+    /**
+     * This method will be called before the processing method.
+     * Intention to establish connection to publish event.
+     * @throws ConnectionUnavailableException if end point is unavailable the ConnectionUnavailableException thrown
+     *                                        such that the  system will take care retrying for connection
+     */
+    @Override
+    public void connect() throws ConnectionUnavailableException {
+
+    }
+
+    /**
+     * Called after all publishing is done, or when {@link ConnectionUnavailableException} is thrown
+     * Implementation of this method should contain the steps needed to disconnect from the sink.
+     */
+    @Override
+    public void disconnect() {
+
+    }
+
+    /**
+     * The method can be called when removing an event receiver.
+     * The cleanups that have to be done after removing the receiver could be done here.
+     */
+    @Override
+    public void destroy() {
+
+    }
+
+    /**
+     * Give information to the deployment about the service exposed by the sink.
+     *
+     * @return ServiceDeploymentInfo  Service related information to the deployment
+     */
+    @Override
+    protected ServiceDeploymentInfo exposeServiceDeploymentInfo() {
+        return null;
+    }
+
+}

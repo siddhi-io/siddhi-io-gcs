30f287ead6169f80781d01a31a8696ceaf3f1fd9
CharukaK <charukakarunanayake@gmail.com>
2019-09-20 17:13:23 +0530

    Add test case of GCSSink

--- component/src/test/java/io/siddhi/extension/io/gcs/sink/TestCaseOfGCSSink.java
+++ component/src/test/java/io/siddhi/extension/io/gcs/sink/TestCaseOfGCSSink.java
@@ -1,6 +1,31 @@
 package io.siddhi.extension.io.gcs.sink;
 
+import io.siddhi.core.SiddhiAppRuntime;
+import io.siddhi.core.SiddhiManager;
+import io.siddhi.core.stream.input.InputHandler;
+import org.testng.annotations.Test;
+
 public class TestCaseOfGCSSink {
-        // If you will know about this related testcase,
-        //refer https://github.com/wso2-extensions/siddhi-io-file/blob/master/component/src/test
+    @Test
+    public void testSQSMessagePublisherInitialization() throws InterruptedException {
+        SiddhiAppRuntime siddhiAppRuntime = null;
+        SiddhiManager siddhiManager = new SiddhiManager();
+
+        try {
+            String streamDef = "@sink(type='google-cloud-storage'," +
+                    " credential.path='<auth.file.path>'," +
+                    " bucket.name='<bucket.name>'," +
+                    " object.name='test-object-{{suffix}}',  @map(type='text') ) \n" +
+                    "define stream outputStream(key string, payload string, suffix string);";
+
+            siddhiAppRuntime = siddhiManager.createSiddhiAppRuntime(streamDef);
+            InputHandler inputHandler = siddhiAppRuntime.getInputHandler("outStream");
+            siddhiAppRuntime.start();
+            inputHandler.send(new Object[]{"message 1", "TestContent", "suffix"});
+        } finally {
+            if (siddhiAppRuntime != null) {
+                siddhiAppRuntime.shutdown();
+            }
+        }
+    }
 }
--- component/src/test/java/io/siddhi/extension/io/gcs/source/TestCaseOfGCSSource.java
+++
@@ -1,6 +0,0 @@
-package io.siddhi.extension.io.gcs.source;
-
-public class TestCaseOfGCSSource {
-        // If you will know about this related testcase,
-        //refer https://github.com/wso2-extensions/siddhi-io-file/blob/master/component/src/test
-}
